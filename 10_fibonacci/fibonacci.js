b76918b6afd5585585ffa506241265f46ade02dc
thecodingliva <xlr8nur@protonmail.com>
2023-09-30 13:24:09 +0300

    second commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    };
+    if (n == 0 || "") {
+        return 0;
+    } if (n == 1 || "") {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
